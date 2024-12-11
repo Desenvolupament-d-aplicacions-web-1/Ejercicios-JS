402fa2cdc0ddd8483e6483bda680062c33cf6a67
𝑱𝒂𝒏 𝑸𝒖𝒆𝒓𝒂𝒍𝒕 <55952925+jaanque@users.noreply.github.com>
2024-12-11 19:14:22 +0100

    Calculadora tabulada

--- Ejercicios-js-parte1/calculadora-js/main.js
+++ Ejercicios-js-parte1/calculadora-js/main.js
@@ -1,33 +1,33 @@
 function calcular() {
-    const numero1 = parseFloat(document.getElementById('numero1').value);
-    const numero2 = parseFloat(document.getElementById('numero2').value);
-    const operacion = document.getElementById('operacion').value;
-    let resultado;
+const numero1 = parseFloat(document.getElementById('numero1').value);
+const numero2 = parseFloat(document.getElementById('numero2').value);
+const operacion = document.getElementById('operacion').value;
+let resultado;
 
-    if (isNaN(numero1) || isNaN(numero2)) {
-        alert("Por favor, ingresa números válidos.");
-        return;
-    }
+if (isNaN(numero1) || isNaN(numero2)) {
+    alert("Por favor, ingresa números válidos.");
+    return;
+}
 
-    switch (operacion) {
-        case 'suma':
-            resultado = numero1 + numero2;
-            break;
-        case 'resta':
-            resultado = numero1 - numero2;
-            break;
-        case 'multiplicacion':
-            resultado = numero1 * numero2;
-            break;
-        case 'division':
-            if (numero2 === 0) {
-                alert("No se puede dividir por cero.");
-                return;
-            }
-            resultado = numero1 / numero2;
-            break;
-        default:
-            resultado = "Operación no válida";
-    }
-    document.getElementById('resultado').textContent = resultado;
+switch (operacion) {
+    case 'suma':
+        resultado = numero1 + numero2;
+        break;
+    case 'resta':
+        resultado = numero1 - numero2;
+        break;
+    case 'multiplicacion':
+        resultado = numero1 * numero2;
+        break;
+    case 'division':
+        if (numero2 === 0) {
+            alert("No se puede dividir por cero.");
+            return;
+        }
+        resultado = numero1 / numero2;
+        break;
+    default:
+        resultado = "Operación no válida";
+}
+document.getElementById('resultado').textContent = resultado;
 }
